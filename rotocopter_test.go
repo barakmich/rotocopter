9d576cfe94b16d50b278eaef723390bc79011b88
Barak Michener <me@barakmich.com>
2020-11-09 23:17:01 -0800

    update testdata to include calling other files

--- rotocopter_test.go
+++ rotocopter_test.go
@@ -10,45 +10,58 @@ import (
 	"github.com/drone/drone-go/plugin/config"
 )
 
-func TestLoadGit(t *testing.T) {
+func loadAndRunTestdata(t *testing.T, req *config.Request) *drone.Config {
 	conf := roto.Config{
 		GitYamlRepo: "testdata/",
 	}
+	r := roto.New(conf)
+	config, err := r.Find(context.Background(), req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return config
+}
+
+func TestLoadGit(t *testing.T) {
 	req := config.Request{
 		Repo: drone.Repo{
 			Slug: "barak/testpipeline",
 		},
 	}
-
-	r := roto.New(conf)
-	config, err := r.Find(context.Background(), &req)
-	if err != nil {
-		t.Fatal(err)
-	}
+	config := loadAndRunTestdata(t, &req)
 	if !strings.Contains(string(config.Data), "Whatup") {
 		t.Fatal("data doesn't contain echo command")
 	}
 	t.Log(config.Data)
 }
 
-func TestLoadStarlark(t *testing.T) {
-	conf := roto.Config{
-		GitYamlRepo: "testdata/",
-	}
+func TestBasicStarlark(t *testing.T) {
 	req := config.Request{
 		Repo: drone.Repo{
 			Slug: "barak/teststarlark",
 		},
 	}
 
-	r := roto.New(conf)
-	config, err := r.Find(context.Background(), &req)
-	if err != nil {
-		t.Fatal(err)
-	}
+	config := loadAndRunTestdata(t, &req)
 	if !strings.Contains(string(config.Data), "kind: docker") {
-		t.Fatal("data doesn't contain echo command")
+		t.Fatal("data doesn't contain docker kind")
 	}
 	t.Log(config.Data)
+}
 
+func TestStarlarkThatLoads(t *testing.T) {
+	req := config.Request{
+		Repo: drone.Repo{
+			Slug: "barak/metatest",
+		},
+	}
+
+	config := loadAndRunTestdata(t, &req)
+	if !strings.Contains(string(config.Data), "kind: docker") {
+		t.Fatal("data doesn't contain docker kind")
+	}
+	if !strings.Contains(string(config.Data), "foobar") {
+		t.Fatal("data doesn't contain foobar step")
+	}
+	t.Log(config.Data)
 }
